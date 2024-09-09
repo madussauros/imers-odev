5506326db0dfde2217c42cfbbc636240e32013d9
madu <104942205+madussauros@users.noreply.github.com>
2024-09-09 16:15:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Omori",
+        descricao: "Omori é um RPG indie que te convida a explorar um mundo onírico e colorido, repleto de personagens cativantes e desafios emocionais. A história gira em torno de Sunny, um jovem que, junto de seu alter ego Omori, embarca em uma jornada introspectiva para lidar com traumas passados. Com uma narrativa envolvente que aborda temas como ansiedade e depressão, Omori oferece uma experiência única.",
+        link: "https://omori.fandom.com/wiki/OMORI"
+    },
+    {
+        titulo: "Celeste",
+        descricao: "Plataforma 2D com foco em progressão desafiadora." ,
+        link: "https://store.steampowered.com/app/504230/Celeste"
+    },
+    {
+        titulo: "Undertale",
+        descricao: "RPG com elementos de aventura e comédia, com foco em decisões do jogador." ,
+        link: "https://store.steampowered.com/app/391540/Undertale/"
+    }
+];
